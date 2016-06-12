89b4cf830942fae8dc5cb96638033698ca3aec8e
Dang Ngoc Duc <dangngocduc.bk@gmail.com>
2016-06-12 13:36:06 +0700

    add file

---
+++ customview/src/main/java/libcommon/dangngocduc/customview/Util.java
@@ -0,0 +1,14 @@
+package libcommon.dangngocduc.customview;
+
+import android.provider.Settings;
+import android.widget.Toast;
+
+/**
+ * Created by DANG NGOC DUC on 6/12/2016.
+ */
+public class Util {
+
+    public static void showToast(){
+        System.out.print("");
+    }
+}
