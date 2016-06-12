8e5dc69bd1d20662c12afa38a0c881f9947697f2
Dang Ngoc Duc <dangngocduc.bk@gmail.com>
2016-06-12 18:20:36 +0700

    Done CirCleImageView

---
+++ anyimageview/src/main/java/android/dangngocduc/anyimageview/CircleImageView.java
@@ -0,0 +1,7 @@
+package android.dangngocduc.anyimageview;
+
+/**
+ * Created by DANG NGOC DUC on 6/12/2016.
+ */
+public class CircleImageView {
+}
---
+++ anyimageview/src/main/java/android/dangngocduc/anyimageview/drawable/BitmapDrawableCircle.java
@@ -0,0 +1,7 @@
+package android.dangngocduc.anyimageview.drawable;
+
+/**
+ * Created by DANG NGOC DUC on 6/12/2016.
+ */
+public class BitmapDrawableCircle {
+}
